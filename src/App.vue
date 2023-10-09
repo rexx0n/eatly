8c0270386f944d6b3fe729911093543b4726175b
rexx0n <alexey.mironenko46@gmail.com>
2023-10-09 14:23:28 +0300

    made page

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
     <router-view></router-view>
 </template>
 
-<style scoped>
-
+<style>
+@keyframes fadein {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+}
 </style>
