59fc4448aeb35f849e9eacbc805775a4c15f2a26
rexx0n <alexey.mironenko46@gmail.com>
2023-10-13 15:29:45 +0300

    made page

--- src/pages/ChoicePage.vue
+++ src/pages/ChoicePage.vue
@@ -93,64 +93,7 @@
             </div>
             <EDishes></EDishes>
         </div>
-        <div class="questions">
-            <div class="questions__title">
-                <img src="../assets/ilustration01.svg" alt="">
-                <h1>Frequently Asked <span>Questions</span></h1>
-            </div>
-            <div class="questions__questions">
-                <div class="questions__question">
-                    <h3>How long does delivery take?
-                        <EButton v-if="!question['one']" @click="onQuestions('one')">+</EButton>
-                        <EButton v-if="question['one']" @click="onQuestions('one')">-</EButton>
-                    </h3>
-                    <div v-if="question['one']">
-                        <p>some text some text some text some text some text some text some text some text some text
-                            some text some text some text some text some text some text some text </p>
-                    </div>
-                </div>
-                <div class="questions__question">
-                    <h3>How Does It Work ?
-                        <EButton v-if="!question['two']" @click="onQuestions('two')">+</EButton>
-                        <EButton v-if="question['two']" @click="onQuestions('two')">-</EButton>
-                    </h3>
-                    <div v-if="question['two']">
-                        <p>some text some text some text some text some text some text some text some text some text
-                            some text some text some text some text some text some text some text </p>
-                    </div>
-                </div>
-                <div class="questions__question">
-                    <h3>How does your food delivery service work?
-                        <EButton v-if="!question['three']" @click="onQuestions('three')">+</EButton>
-                        <EButton v-if="question['three']" @click="onQuestions('three')">-</EButton>
-                    </h3>
-                    <div v-if="question['three']">
-                        <p>some text some text some text some text some text some text some text some text some text
-                            some text some text some text some text some text some text some text </p>
-                    </div>
-                </div>
-                <div class="questions__question">
-                    <h3>What payment options do you accept?
-                        <EButton v-if="!question['four']" @click="onQuestions('four')">+</EButton>
-                        <EButton v-if="question['four']" @click="onQuestions('four')">-</EButton>
-                    </h3>
-                    <div v-if="question['four']">
-                        <p>some text some text some text some text some text some text some text some text some text
-                            some text some text some text some text some text some text some text </p>
-                    </div>
-                </div>
-                <div class="questions__question">
-                    <h3>Do you offer discounts or promotions?
-                        <EButton v-if="!question['five']" @click="onQuestions('five')">+</EButton>
-                        <EButton v-if="question['five']" @click="onQuestions('five')">-</EButton>
-                    </h3>
-                    <div v-if="question['five']">
-                        <p>some text some text some text some text some text some text some text some text some text
-                            some text some text some text some text some text some text some text </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <EQuestions></EQuestions>
     </div>
     <EFooter></EFooter>
 </template>
@@ -163,23 +106,15 @@ import EButton from "../components/UI/EButton.vue";
 import EView from "../components/UI/EView.vue";
 import ERestaurants from "../components/UI/ERestaurants.vue";
 import EDishes from "../components/UI/EDishes.vue";
+import EQuestions from "../components/UI/EQuestions.vue";
 
 let translate = ref(0)
 let count = ref(1)
 let choice = ref(2)
 let icon = ref(2)
-let question = ref({
-    'one': false,
-    'two': false,
-    'three': false,
-    'four': false,
-    'five': false,
 
-})
 
-function onQuestions(value) {
-    question.value[value] = !question.value[value]
-}
+
 
 function slider() {
     if (count.value <= 2) {
--- src/pages/FoodPage.vue
+++ src/pages/FoodPage.vue
@@ -13,62 +13,102 @@
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="62" height="62" viewBox="0 0 62 62" fill="none">
                         <circle cx="30.8916" cy="30.8916" r="30.8916" fill="#DBD9EE"/>
-                        <path d="M37.6606 16.452C33.1884 15.7022 28.5985 15.7022 24.1263 16.452C22.4925 16.7259 21.1913 17.9802 20.8637 19.6065C19.363 27.0572 19.2811 34.724 20.6224 42.205L21.1685 45.2508C21.3368 46.19 22.4818 46.5653 23.1735 45.9082L29.7651 39.6462C30.3974 39.0455 31.3895 39.0455 32.0218 39.6462L38.6134 45.9082C39.3051 46.5653 40.4501 46.19 40.6184 45.2508L41.1645 42.205C42.5058 34.724 42.4239 27.0572 40.9232 19.6065C40.5956 17.9802 39.2944 16.7259 37.6606 16.452Z" fill="#6C5FBC"/>
+                        <path d="M37.6606 16.452C33.1884 15.7022 28.5985 15.7022 24.1263 16.452C22.4925 16.7259 21.1913 17.9802 20.8637 19.6065C19.363 27.0572 19.2811 34.724 20.6224 42.205L21.1685 45.2508C21.3368 46.19 22.4818 46.5653 23.1735 45.9082L29.7651 39.6462C30.3974 39.0455 31.3895 39.0455 32.0218 39.6462L38.6134 45.9082C39.3051 46.5653 40.4501 46.19 40.6184 45.2508L41.1645 42.205C42.5058 34.724 42.4239 27.0572 40.9232 19.6065C40.5956 17.9802 39.2944 16.7259 37.6606 16.452Z"
+                              fill="#6C5FBC"/>
                     </svg>
                 </div>
             </div>
         </div>
+        <div class="dishes">
+            <h1>Popular 🔥</h1>
+            <EDishes></EDishes>
+            <div class="dishes__chicken">
+                <h1>Chicken Vegetables</h1>
+                <div class="dishes__cards dishes__cards--food">
+                    <div class="dishes__card">
+                        <img @click="heart = 2" v-if="heart !== 2" src="../assets/heart.svg" alt="heart">
+                        <img @click="heart = 3" v-else src="../assets/heart2.png" alt="heart">
+                        <img src="../assets/food-image4.png" alt="foodimage">
+                        <div class="dishes__info">
+                            <span class="dishes__healthy dishes__healthy--trending">Trending</span>
+                            <h4>Swe Dish</h4>
+                            <div class="dishes__feedback">
+                                <span>34min •</span>
+                                <img src="../assets/star1.svg" alt="">
+                                <span>4.8</span>
+                            </div>
+                            <div class="dishes__price">
+                                <h3>$19<span>.99</span></h3>
+                                <button @click="toNextPage">+</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="dishes__card">
+                        <img @click="heart = 4" v-if="heart !== 4" src="../assets/heart.svg" alt="heart">
+                        <img @click="heart = 5" v-else src="../assets/heart2.png" alt="heart">
+                        <img src="../assets/food-image5.png" alt="foodimage">
+                        <div class="dishes__info">
+                            <span class="dishes__healthy dishes__healthy--supreme">Supreme</span>
+                            <h4>Swe Dish</h4>
+                            <div class="dishes__feedback">
+                                <span>24min •</span>
+                                <img src="../assets/star1.svg" alt="">
+                                <span>4.9</span>
+                            </div>
+                            <div class="dishes__price">
+                                <h3>$19<span>.99</span></h3>
+                                <button @click="toNextPage">+</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <EQuestions></EQuestions>
     </div>
     <EFooter></EFooter>
 </template>
 
-<script>
+<script setup>
 import EHeader from "../components/UI/EHeader.vue";
 import EFooter from "../components/UI/EFooter.vue";
+import EDishes from "../components/UI/EDishes.vue";
+import {ref} from "vue";
+import router from "../router/router.js";
+import EQuestions from "../components/UI/EQuestions.vue";
 
-export default {
-    name: "FoodPage",
-    components: {EFooter, EHeader}
+let heart = ref(true)
+
+function toNextPage() {
+    router.push({
+        name: 'choice'
+    })
 }
 </script>
 
 <style scoped lang="scss">
-.chicken {
-    margin-top: 106px;
-   max-width: 1238px;
-    &__card {
-        border-radius: 30px;
-        border: 1.5px solid #F4F4F6;
-        background: #FFF;
-        box-shadow: 7.5px 91px 45px 0px rgba(229, 229, 229, 0.70);
+.questions {
+    button {
+        padding: 1px 8px;
+        border-radius: 20px;
     }
-    &__text {
-        display: flex;
-        padding: 26px 30px 31px 46px;
-        border-radius: 30px;
-        background:  #FFF;
-        box-shadow: 7.5px 91px 45px 0px rgba(229, 229, 229, 0.70);
-        span {
-            color: #8E97A6;
-            font-family: 'Manrope',sans-serif;
-            font-size: 26px;
-            font-style: normal;
-            font-weight: 400;
-            line-height: 35px; /* 133.333% */
-        }
-        h1 {
-            color: #323142;
-            font-size: 40px;
-            font-style: normal;
-            font-weight: 600;
-            line-height: 53px; /* 133.41% */
-        }
-        div {
-            display: flex;
-            align-items: center;
-            margin-left: clamp(121px, 20vw, 170px);
-            margin-right: clamp(267px, 30vw, 376px);
-        }
+}
+.dishes {
+  margin-top: 148px;
+    &__cards--food {
+        justify-content: flex-start;
+        gap: 31px;
     }
+    &__chicken {
+        margin-top: 147px;
+    }
+  h1 {
+    color: #323142;
+    font-size: 35px;
+    text-align: start;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 25px; /* 73.024% */
+  }
 }
 </style>
