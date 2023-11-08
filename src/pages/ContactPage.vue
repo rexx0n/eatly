6b5c224555cd610992b507c080e2f368dcddee14
rexx0n <alexey.mironenko46@gmail.com>
2023-11-08 21:40:53 +0300

    small changes

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
       <EHeader active="contact"></EHeader>
+      <div class="contact">
+          <div class="contact__just">
+            <h1>Just Contact</h1>
+          </div>
+          <div class="contact__support">
+
+          </div>
+      </div>
   </div>
   <EFooter></EFooter>
 </template>
@@ -11,6 +19,17 @@ import EHeader from "../components/UI/EHeader.vue";
 import EFooter from "../components/UI/EFooter.vue";
 </script>
 
-<style scoped>
+<style lang="scss">
+.contact {
+  &__just {
+    h1 {
+      color: #F9F9F9;
+      font-size: 48px;
+      font-weight: 600;
+      line-height: 105%; /* 50.4px */
+      box-shadow: 1px 1px 1px #6C5FBC;
+    }
 
+  }
+}
 </style>
