3dcf73864c98aa90dab1b92da55b3f99399b5ebb
rexx0n <alexey.mironenko46@gmail.com>
2023-12-08 17:51:34 +0300

    work with 375 resolution

--- src/components/UI/EHeader.vue
+++ src/components/UI/EHeader.vue
@@ -5,6 +5,25 @@
               <img src="../../assets/logo.svg" alt="eatly">
               <span>eatly</span>
           </div>
+          <div class="header__burger">
+              <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="34" height="25" viewBox="0 0 34 25" fill="none">
+                      <path d="M1.25 1H32.75H1.25Z" fill="#918F8F"/>
+                      <path d="M1.25 1H32.75" stroke="#282828" stroke-width="2" stroke-linecap="round"/>
+                      <path d="M1.25 12.1628H32.75H1.25Z" fill="#918F8F"/>
+                      <path d="M1.25 12.1628H32.75" stroke="#282828" stroke-width="2" stroke-linecap="round"/>
+                      <path d="M1.25 23.3257H32.75H1.25Z" fill="#918F8F"/>
+                      <path d="M1.25 23.3257H32.75" stroke="#282828" stroke-width="2" stroke-linecap="round"/>
+                  </svg>
+              </div>
+              <nav>
+                  <a @click="toNext('choice')" :class="{header__active: links.menu}" href="">Menu</a>
+                  <a @click="toNext('articles')" :class="{header__active: links.blog}" href="">Blog</a>
+                  <a @click="toNext('pricing')" :class="{header__active: links.pricing}" href="">Pricing</a>
+                  <a  @click="toNext('contact')" :class="{header__active: links.contact}" href="">Contact</a>
+              </nav>
+          </div>
+
           <nav>
               <a @click="toNext('choice')" :class="{header__active: links.menu}" href="">Menu</a>
               <a @click="toNext('articles')" :class="{header__active: links.blog}" href="">Blog</a>
